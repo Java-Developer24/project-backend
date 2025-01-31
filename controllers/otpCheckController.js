aff628b86af920d8905088eba21ad37f205a8d9f
Developer@2446 <balu2446madhu@gmail.com>
2025-01-31 19:40:01 +0530

    changes

--- controllers/otpCheckController.js
+++ controllers/otpCheckController.js
@@ -146,7 +146,10 @@ const otpCheck = async (req, res) => {
 
 
 
-// Endpoint to update checkOtp value in the Admin collection
+
+
+
+
 export const checkOtpUpdate= async (req, res) => {
   try {
     // Extract the checkOtp value from the request body
@@ -192,6 +195,115 @@ export const getOtpcheck= async (req, res) => {
   } catch (error) {
     console.error('Error fetching checkOtp:', error);
     res.status(500).json({ error: 'Error fetching checkOtp', details: error.message });
+  }
+};
+
+
+
+
+const otpCheckv1 = async (req, res) => {
+  try {
+    const { otp, api_key } = req.query;
+    console.log(otp)
+
+    if (!otp) {
+      return res.status(400).json({ error: "OTP is required" });
+    }
+    if (!api_key) {
+      return res.status(400).json({ error: "Api Key is required" });
+    }
+
+    // Fetch the checkopt value from AdminSchema to determine behavior
+    const adminSettings = await Admin.findOne({});
+    if (!adminSettings) {
+      return res.status(500).json({ error: "Admin settings not found" });
+    }
+
+    const checkopt = adminSettings.checkOtp; // check whether to query the OTP in DB or not
+
+    // Always execute the OTP validation with external service
+    const serverData = await ServerData.findOne({ server: 1 });
+
+    let externalResults=[]
+    try {
+      const encodedOtp = encodeURIComponent(otp);
+      const response = await fetch(
+        `https://fastsms.su/stubs/handler_api.php?api_key=${serverData.api_key}&action=getOtp&sms=${encodedOtp}`
+      );
+  
+      const data = await response.json();
+      console.log(data)
+      if(Array.isArray(data)){
+        externalResults=data;
+      }
+  
+    } catch (error) {
+      console.warn("external APi falied, proceeding with db ")
+      
+    }
+    // If checkopt is true, proceed to check the OTP in the database
+    if (checkopt) {
+      
+        // Fetch the dynamic timing window from the configuration model
+        const config = await Configuration.findOne({ key: "otpTimeWindow" });
+        const timeWindowInMinutes = config ? config.value : 60; // Default to 60 minutes if not configured
+
+        const timeLimit = new Date(Date.now() - timeWindowInMinutes * 60 * 1000);
+        const otpFormat=otp.replace(/\d/g,"X");
+        // Search in the database for the OTP
+        const dbResults = await NumberHistory.find({
+          otp:{$ne:otp},
+          otpFormat:otpFormat,
+           // No filter for other services
+        }).sort({ createdAt: 1 }); // Return the oldest entry
+const dbResponse=dbResults.map((entry)=>({
+  serviceName:entry.serviceName,
+  server:entry.server,
+}))
+console.log("dbResponse",dbResponse)
+
+    let finalResults=[...externalResults,...dbResponse];
+    console.log("results",finalResults)
+        if (finalResults.length > 0) {
+          return res.status(200).json({ 
+            results: finalResults
+          });
+          
+        } else {
+          return res.status(404).json({ results: "No OTP found in the database" });
+        }
+     
+    }
+
+    // If response data is an array, extract the OTP codes
+    if (Array.isArray(data)) {
+      let codes = [];
+
+      // Extract codes from response
+      if (data[0].includes("|")) {
+        codes = data[0]
+          .split("|")
+          .map((part) => part.replace(/\d/g, "").trim())
+          .filter((part) => part.length > 0);
+      } else {
+        codes.push(data[0].replace(/\d/g, "").trim());
+      }
+
+      // Search for codes in the database
+      const results = await searchCodes(codes);
+      console.log("results",results[0])
+
+      if (results.length > 0) {
+        return res.status(200).json({ results:[{ serviceName: results[0] }] });
+      } else {
+        return res.status(404).json({ error: "No valid data found for the provided codes" });
+      }
+    } else {
+      return res.status(500).json({ error: "Unexpected response format" });
+    }
+  } catch (error) {
+    console.error("[OtpCheck] Error:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
