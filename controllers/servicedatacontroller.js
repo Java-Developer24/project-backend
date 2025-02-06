e77392585a0925f0ceb738fbe6dc0ca2f8dd7298
Developer@2446 <balu2446madhu@gmail.com>
2025-02-06 08:47:26 +0530

    changes

--- controllers/servicedatacontroller.js
+++ controllers/servicedatacontroller.js
@@ -449,7 +449,7 @@ const checkServiceAvailability = async (sname, server) => {
       
   
       const formattedDateTime = moment().tz("Asia/Kolkata").format("DD/MM/YYYY HH:mm:ss A");
-      const uniqueID = `${moment().tz("Asia/Kolkata").format("DDMMYYYYHHmmssSSS")}${uuidv4().slice(0, 8)}${Math.floor(Math.random() * 10000)}${process.pid}`;
+      const uniqueID = moment().tz("Asia/Kolkata").format("DDMMYYYYHHmmss");
 
          const Id = uniqueID;
       
